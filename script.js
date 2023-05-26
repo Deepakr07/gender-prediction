69871ae1dba9347df5d6f6c215d4fbd265577fc2
deepak <deepakrajesh567@gmail.com>
2023-05-26 09:33:15 +0530

    Fetched details using API

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const mainUrl = "https://api.genderize.io?name="
+const url = "https://api.genderize.io?name="
 let wrapper = document.getElementById("wrapper");
 let button = document.getElementById("prediction-btn");
 let predictGender = () =>{
-    const name = document.getElementById("user-id").value
+    let name = document.getElementById("user-id").value
+    let finalUrl = url+name;
+    let error = document.getElementById("error");
+    if(name.length>0 && /^[A-Za-z]+$/.test(name)){
+        fetch(finalUrl)
+            .then((resp) => resp.json())
+            .then((data) =>{
+                console.log(data);
+    let div = document.createElement("div");
+    div.setAttribute("id","info");
+    div.innerHTML = `<h2 id = "result-name">${data.name}</h2><img src = "" id = "gender-icon"><h1 id = "gender" >${data.gender}</h1><h4 id = "probability">Probability :${data.probability}</h4>`
+    wrapper.appendChild(div);
+            })
     
-    console.log(name);
+            
+    }
 }
 window.addEventListener("load",predictGender);
 button.addEventListener("click",predictGender);
